8e0693937e15164d3f000f63afa53fec9c9b2a60
reklai <official.reklai@gmail.com>
2025-11-30 18:11:54 +0900

    get chirps

--- router.go
+++ router.go
@@ -16,6 +16,13 @@ import (
 type Router struct {
 	Mux *http.ServeMux
 }
+type SingleChirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
 	response, err := json.Marshal(payload)
@@ -34,14 +41,17 @@ func respondWithError(w http.ResponseWriter, code int, msg string) error {
 
 func (router *Router) handlers(apiCfg *apiConfig, staticDir string, headerMethod map[string]string, endPoints map[string]string) {
 	// App Endpoints ------------------------------------------------------------------------------------------------------
+
 	router.Mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(staticDir)))))
 
 	// Api Endpoints --------------------------------------------------------------------------------------------------------------------------------
+
 	router.Mux.HandleFunc(fmt.Sprintf("%s %s%s", headerMethod["GET"], endPoints["api"], "/healthz"), func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("content-type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		io.WriteString(w, "OK")
 	})
+
 	router.Mux.HandleFunc(fmt.Sprintf("%s %s%s", headerMethod["POST"], endPoints["api"], "/users"), func(w http.ResponseWriter, req *http.Request) {
 		defer req.Body.Close()
 		chirpUser := struct {
@@ -55,12 +65,12 @@ func (router *Router) handlers(apiCfg *apiConfig, staticDir string, headerMethod
 		}{}
 		body, err := io.ReadAll(req.Body)
 		if err != nil {
-			respondWithError(w, 400, "could not read request body")
+			respondWithError(w, 400, "failed to read request body")
 			return
 		}
 		err = json.Unmarshal(body, &chirpUser)
 		if err != nil {
-			respondWithError(w, 400, "could not read request body")
+			respondWithError(w, 400, "failed to unmarshal request")
 			return
 		}
 		user, err := apiCfg.databaseQuery.CreateUser(req.Context(), database.CreateUserParams{ID: uuid.New(), Email: chirpUser.Email})
@@ -74,12 +84,31 @@ func (router *Router) handlers(apiCfg *apiConfig, staticDir string, headerMethod
 		userJSON.Email = user.Email
 		response, err := json.Marshal(userJSON)
 		if err != nil {
-			respondWithError(w, 400, "failed to translate into bytes")
+			respondWithError(w, 400, "failed to marshal json into bytes")
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(201)
 		w.Write(response)
+	})
+
+	router.Mux.HandleFunc(fmt.Sprintf("%s %s%s", headerMethod["GET"], endPoints["api"], "/chirps"), func(w http.ResponseWriter, req *http.Request) {
+		defer req.Body.Close()
+		var responseChirp SingleChirp
+		chirps, err := apiCfg.databaseQuery.GetAllChirps(req.Context())
+		if err != nil {
+			respondWithError(w, 500, "failed to get all chirps")
+		}
+		allChirps := make([]SingleChirp, 0)
+		for _, value := range chirps {
+			responseChirp.ID = value.ID
+			responseChirp.CreatedAt = value.CreatedAt
+			responseChirp.UpdatedAt = value.UpdatedAt
+			responseChirp.Body = value.Body
+			responseChirp.UserID = value.UserID
+			allChirps = append(allChirps, responseChirp)
+		}
+		respondWithJSON(w, 200, allChirps)
 	})
 	router.Mux.HandleFunc(fmt.Sprintf("%s %s%s", headerMethod["POST"], endPoints["api"], "/chirps"), func(w http.ResponseWriter, req *http.Request) {
 		defer req.Body.Close()
@@ -94,13 +123,13 @@ func (router *Router) handlers(apiCfg *apiConfig, staticDir string, headerMethod
 
 		body, err := io.ReadAll(req.Body)
 		if err != nil {
-			respondWithError(w, 400, "could not read request body")
+			respondWithError(w, 400, "failed to read request body")
 			return
 		}
 
 		err = json.Unmarshal(body, &chirpProfanity)
 		if err != nil {
-			respondWithError(w, 400, "could not unmarshal request")
+			respondWithError(w, 400, "failed to unmarshal request")
 			return
 		}
 
@@ -151,12 +180,15 @@ func (router *Router) handlers(apiCfg *apiConfig, staticDir string, headerMethod
 		respondWithJSON(w, 201, chirpClean)
 
 	})
+
 	// Admin Endpoints --------------------------------------------------------------------------------------------------------------------------------
+
 	router.Mux.HandleFunc(fmt.Sprintf("%s %s%s", headerMethod["GET"], endPoints["admin"], "/metrics"), func(w http.ResponseWriter, req *http.Request) {
 		metricHTML := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %v times!</p></body></html>", apiCfg.metrics())
 		w.WriteHeader(200)
 		io.WriteString(w, metricHTML)
 	})
+
 	router.Mux.HandleFunc(fmt.Sprintf("%s %s%s", headerMethod["POST"], endPoints["admin"], "/reset"), func(w http.ResponseWriter, req *http.Request) {
 		if apiCfg.platform != "dev" {
 			respondWithError(w, 403, "something went wrong")
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -3,5 +3,8 @@ INSERT INTO chirps(body, user_id)
 VALUES($1, $2)
 RETURNING *;
 
+-- name: GetAllChirps :many
+SELECT * FROM chirps;
+
 -- name: DeleteUserChirp :exec
 DELETE FROM chirps WHERE id = $1;
